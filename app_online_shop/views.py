060ec39c41bbaf58c24d10f071a6c54a6ebb8675
dolezha <od10632@gmail.com>
2023-08-12 20:17:49 +0300

    commit

---
+++ app_online_shop/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+# подключаем объект для выполнения http запросов
+from django.http import HttpResponse
+
+# Create your views here.
+
+#функция, отображающая файл index.html
+def index(request):
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
